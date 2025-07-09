0adfed69b7e0eb4b97003d5550880371e5a1c10a
bast1989 <125972050+bast1989@users.noreply.github.com>
2025-07-09 22:56:05 +0700

    Update 2 main
    Создание экземпляра класса bot = ptbot.Bot(TG_TOKEN) переехало в функцию main()

--- main.py
+++ main.py
@@ -14,38 +14,53 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def reply(seconds_left, chat_id, message_id, total_seconds):
-    progress_bar = render_progressbar(total_seconds, total_seconds - seconds_left)
-    bot.update_message(chat_id, message_id, f'Осталось: {seconds_left} секунд\n{progress_bar}')
+def reply(seconds_left, bot, chat_id, message_id, total_seconds):
+    progress_bar = render_progressbar(
+        total_seconds,
+        total_seconds - seconds_left
+    )
+    bot.update_message(
+        chat_id,
+        message_id,
+        f'Осталось: {seconds_left} секунд\n{progress_bar}'
+    )
 
 
-def finish_counting(chat_id, original_message):
+def finish_counting(bot, chat_id, original_message):
     bot.send_message(chat_id, 'Время вышло.')
 
 
-def notify_progress(author_id, message_text):
+def notify_progress(bot, author_id, message_text):
     total_seconds = parse(message_text)
     message_id = bot.send_message(author_id, 'Запускаю таймер')
+
     bot.create_countdown(
         total_seconds,
         reply,
+        bot=bot,
         chat_id=author_id,
         message_id=message_id,
         total_seconds=total_seconds
     )
+
     bot.create_timer(
         total_seconds,
         finish_counting,
+        bot=bot,
         chat_id=author_id,
         original_message=message_text
     )
 
 
 def main():
-    bot.reply_on_message(notify_progress)
+    bot = ptbot.Bot(TG_TOKEN)
+
+    def forwarding_an_instance(chat_id, text):
+        notify_progress(bot, chat_id, text)
+
+    bot.reply_on_message(forwarding_an_instance)
     bot.run_bot()
 
 
 if __name__ == '__main__':
-    bot = ptbot.Bot(TG_TOKEN)
     main()
